4343826829ff8d8884fd01289d3f7a4c4dbe8406
xinjian.wu <xinjian.wu@chinaredstar.com>
2017-07-02 17:02:15 +0800

    spring boot 整合mybatis

---
+++ src/main/java/com/swellwu/dao/ScoreMapper.java
@@ -0,0 +1,31 @@
+package com.swellwu.dao;
+
+import java.util.List;
+
+import com.swellwu.po.Score;
+import com.swellwu.po.ScoreExample;
+import org.apache.ibatis.annotations.Param;
+
+public interface ScoreMapper {
+    long countByExample(ScoreExample example);
+
+    int deleteByExample(ScoreExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Score record);
+
+    int insertSelective(Score record);
+
+    List<Score> selectByExample(ScoreExample example);
+
+    Score selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Score record, @Param("example") ScoreExample example);
+
+    int updateByExample(@Param("record") Score record, @Param("example") ScoreExample example);
+
+    int updateByPrimaryKeySelective(Score record);
+
+    int updateByPrimaryKey(Score record);
+}
---
+++ src/main/java/com/swellwu/po/Student.java
@@ -0,0 +1,43 @@
+package com.swellwu.po;
+
+public class Student {
+    private Integer id;
+
+    private String name;
+
+    private Byte age;
+
+    private Byte sex;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Byte getAge() {
+        return age;
+    }
+
+    public void setAge(Byte age) {
+        this.age = age;
+    }
+
+    public Byte getSex() {
+        return sex;
+    }
+
+    public void setSex(Byte sex) {
+        this.sex = sex;
+    }
+}
